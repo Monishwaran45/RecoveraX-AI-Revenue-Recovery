15294cd1a3a2581dd22eb0220cc9352b2a822348
Monishwaran45 <monishlegend1780@gmail.com>
2026-08-30 10:25:34 +0530

    fix(cors): Allow PUT and DELETE HTTP methods in CORSMiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,8 +42,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.middleware("http")
